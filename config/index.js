bc4925d929f2c8ac38e3126535448879a5770e2a
1224374619 <1224374619@qq.com>
2019-11-08 11:19:19 +0800

    12

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '',
-    assetsPublicPath: './',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/vue-admin/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -25,8 +25,8 @@ module.exports = {
     env: require('./dev.env'),
     port: 8082,
     autoOpenBrowser: true,
-    assetsSubDirectory: '',
-    assetsPublicPath: './',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     proxyTable: {
       '/apis': {
         // 测试环境
